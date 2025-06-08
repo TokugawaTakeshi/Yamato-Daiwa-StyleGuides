935967169e4b936dfa164089f53d8a6d397516cb
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2025-06-08 12:26:16 +0900

    0.6.10

--- Package/Tests/TypeScriptTest.ts
+++ Package/Tests/TypeScriptTest.ts
@@ -1,3 +1,8 @@
+import RawConfig from "eslint-plugin-react/lib/rules/jsx-no-literals";
+
+
+let bla: RawConfig;
+
 const a: string = "FOO"
 
 class Test {
@@ -15,3 +20,26 @@ const bla: bigint = 1243;
 setTimeout((): void => {}, 1000);
 
 let test: BufferEncoding | undefined;
+
+
+async function asyncFunction(): Promise<void> {
+  return new Promise<void>((resolve: () => void): void => {
+    setTimeout(resolve, 1000);
+  });
+}
+
+
+class TestClass {
+
+  public static async test(): Promise<void> {
+    return asyncFunction();
+  }
+
+}
+
+
+let aaaa: boolean | undefined;
+
+if (aaaa) {
+
+}
